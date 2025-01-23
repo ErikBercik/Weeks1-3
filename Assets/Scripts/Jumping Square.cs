ed756bbf8fcf647e512c55c35002a7a05ad1ef68
Erik Bercik <bercik@shernet.sheridancollege.ca>
2025-01-23 15:56:31 -0500

    IT ROTATES AND JUMPS I AM MAGIC MAN TONY HAWK

--- Assets/Scripts/Jumping Square.cs
+++ Assets/Scripts/Jumping Square.cs
@@ -7,11 +7,13 @@ public class JumpingSquare : MonoBehaviour
 {
     private float speed = 0.02f;
     private float jumpingHeight = 3f;
+    public float rotateAmount = 100f;
 
     [Range(0, 1)]
     public float t;
 
     public AnimationCurve jumpCurve;
+    public AnimationCurve spinzCurve;
 
     bool spacePressed = false;
 
@@ -29,21 +31,36 @@ public class JumpingSquare : MonoBehaviour
         pos.x += speed;
         pos.y = jumpCurve.Evaluate(t) * jumpingHeight;
 
+        Vector3 rot = transform.eulerAngles;
+        rot.z = spinzCurve.Evaluate(t) * rotateAmount;
+
         Vector2 screenPos = Camera.main.WorldToScreenPoint(pos);
 
-        if(screenPos.x < 0 || screenPos.x > Screen.width)
+        if (screenPos.x < 0 || screenPos.x > Screen.width)
         {
             speed = speed * -1;
         }
 
-        if()
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            spacePressed = true;
+        }
 
-        if(spacePressed == true)
+        if (spacePressed == true)
         { 
             t += Time.deltaTime;
         }
 
+        if (t > 1)
+        {
+            t = 0;
+            spacePressed = false;
+        }
+
         transform.position = pos;
+        transform.eulerAngles = rot;
+
+        
  
     }
 }
